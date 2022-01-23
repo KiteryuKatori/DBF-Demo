58acef9959a1cb3a13002072a9cf142d1e3ea7dd
KiteryuKatori <tsfhbc@gmail.com>
2022-01-24 01:12:53 +0700

    AboutUs 90%, just the animations are needed to fix, 2 days left

--- Js/home.js
+++ Js/home.js
@@ -1,40 +1,49 @@
-const pane = document.querySelectorAll(".pane");
+const coreTeamDropDownBtn = document.querySelector('#dropdownIconContainer');
+const CoreTeam = document.querySelector('#CoreTeam');
+const coreTeamList = document.querySelector('.coreTeamList');
 
-const info = document.querySelector(".info");
+coreTeamDropDownBtn.addEventListener("click", () => {
 
-
-pane.forEach(panel => {
-    panel.addEventListener("click", () => {
-        const activatedPanel =  panel.closest(".navBar").querySelector(".active");
-        deActivate(activatedPanel);
-        Activate(panel);
-    })
+    if (!CoreTeam.classList.contains("active")) {
+        addClass(coreTeamDropDownBtn, "active");
+        addClass(CoreTeam, "active");
+        addClass(coreTeamList, "active");
+    }
+    else {
+        remClass(coreTeamDropDownBtn, "active");
+        remClass(CoreTeam, "active");
+        remClass(coreTeamList, "active");
+    }
 })
 
-function deActivate(panel) {
-    if (panel == null) {
-        return;
-    }
-    panel.classList.remove("active");
-    console.log("Deactivate panel");
-}
 
-function Activate(panel) {
-    if (panel == null) {
-        return;
-    }
-    panel.classList.add("active");
-    console.log("Activate panel");
-    if( panel.innerHTML == "about.") {
-        //info.innerHTML = "Đây là section nội dung của about";
-        info.innerHTML = "www.google.com";
-        
-    }else if( panel.innerHTML == "donate.") {
-        info.innerHTML = "Đây là section nội dung của donate. Sau này sẽ add thêm 1 chiếc button(hoặc link) xinh xắn để dẫn người dùng qua donating page";
-        
-    }else if( panel.innerHTML == "license.") {
-        info.innerHTML = "Đây là section nội dung của license. Cái này anh chưa biết gì nên chưa có idea gì hết nhe =)))";
-    }else if( panel.innerHTML == "faq.") {
-        info.innerHTML = "- faq: phần mô tả sẽ là các chủ đề chính(3-5 chủ đề), click vào chủ đề nào thì sẽ hiện ra page về chủ đề đó + 7749 các câu hỏi thường gặp";
-    }
-}
+function addClass(e, c) {e.classList.add(c);}
+function remClass(e, c) {e.classList.remove(c);}
+
+// function cellSelect(cell) {
+//     if (cell == null) return;
+//     if(!cell.classList.contains("selected")) {
+//         cell.classList.add("selected");
+//     }
+//     else {
+//         cell.classList.remove("selected");
+//     }
+// }
+
+// closeModalList.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         //console.log(`${btn.parentElement}`);
+//         closeModal(document.querySelector(`#${btn.parentElement.parentElement.id}`));
+//     })
+// })
+
+// function closeModal(modal) {
+//     modal.parentElement.classList.remove("active");
+//     modal.classList.remove("active");
+// }
+
+// function openModal(modal) {
+//     modal.parentElement.classList.add("active");
+//     modal.classList.add("active");
+//     // console.log(`${modal} activated`);
+// }
