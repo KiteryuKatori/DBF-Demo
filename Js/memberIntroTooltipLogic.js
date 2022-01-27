cef847bbdf6fe4d492f666236df73e5923b43df5
Zen-net <allyofjustice1@gmail.com>
2022-01-27 23:55:46 +0700

    change content

--- Js/memberIntroTooltipLogic.js
+++ Js/memberIntroTooltipLogic.js
@@ -3,12 +3,14 @@ document.querySelectorAll('.smolCell').forEach((el) => {
     el.classList.add(uniqIdent)
 
     const contentDOM = el.querySelector('.quoteModal .quote')
-    tippy(`.${uniqIdent}`, {
-        content: contentDOM ? `<div class="tooltip-sr"><p class="tooltip-text">${contentDOM.innerText}</p></div>` : 'My tooltip!',
-        animateFill: true,
-        animation: 'fade',
-        delay: 0,
-        duration: [0, 0],
-        allowHTML: true,
-    });
+    // currently, does not have detail yet
+
+    // tippy(`.${uniqIdent}`, {
+    //     content: contentDOM ? `<div class="tooltip-sr"><p class="tooltip-text">${contentDOM.innerText}</p></div>` : 'My tooltip!',
+    //     animateFill: true,
+    //     animation: 'fade',
+    //     delay: 0,
+    //     duration: [0, 0],
+    //     allowHTML: true,
+    // });
 })
