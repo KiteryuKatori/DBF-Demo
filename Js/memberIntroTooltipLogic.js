33d023ad9c1408cd06143496e58c11cc8a4731f4
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-03-12 17:11:43 +0700

    update CoreTeamList(change to grid and responsive + still got problems with imgs)

--- Js/memberIntroTooltipLogic.js
+++ Js/memberIntroTooltipLogic.js
@@ -1,16 +1,16 @@
-document.querySelectorAll('.smolCell').forEach((el) => {
-    const uniqIdent = makeID(10)
-    el.classList.add(uniqIdent)
+// document.querySelectorAll('.smolCell').forEach((el) => {
+//     const uniqIdent = makeID(10)
+//     el.classList.add(uniqIdent)
 
-    const contentDOM = el.querySelector('.quoteModal .quote')
-    // currently, does not have detail yet
+//     const contentDOM = el.querySelector('.quoteModal .quote')
+//     // currently, does not have detail yet
 
-    // tippy(`.${uniqIdent}`, {
-    //     content: contentDOM ? `<div class="tooltip-sr"><p class="tooltip-text">${contentDOM.innerText}</p></div>` : 'My tooltip!',
-    //     animateFill: true,
-    //     animation: 'fade',
-    //     delay: 0,
-    //     duration: [0, 0],
-    //     allowHTML: true,
-    // });
-})
+//     // tippy(`.${uniqIdent}`, {
+//     //     content: contentDOM ? `<div class="tooltip-sr"><p class="tooltip-text">${contentDOM.innerText}</p></div>` : 'My tooltip!',
+//     //     animateFill: true,
+//     //     animation: 'fade',
+//     //     delay: 0,
+//     //     duration: [0, 0],
+//     //     allowHTML: true,
+//     // });
+// })
